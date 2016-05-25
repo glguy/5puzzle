c175398e47ba98fc58e39291c4f2fa7586560c13
Eric Mertens <emertens@gmail.com>
2016-05-24 17:16:11 -0700

    Improve runSelect speed

--- src/Select.hs
+++ src/Select.hs
@@ -11,7 +11,7 @@ import FromBit
 import Data.Maybe
 import Data.List (findIndex)
 import Control.Monad.State
-import Prelude hiding (or, (&&))
+import Prelude hiding (and, or, (&&), (||) ,not)
 
 -- | A set of choices and an index of the chosen element of that set
 data Select a = Selected a | Choice Bit (Select a) (Select a)
@@ -21,18 +21,26 @@ select xs = merge (map Selected xs)
   where
   merge []  = error "select: empty list"
   merge [x] = return x
-  merge xs  = do b   <- exists
-                 xs' <- reduce b xs
-                 merge xs'
+  merge xs  = merge =<< reduce xs =<< exists
 
-  reduce b (x1:x2:xs) =
-     do xs' <- reduce b xs
+  reduce (x1:x2:xs) b =
+     do xs' <- reduce xs b
         return (Choice b x1 x2 : xs')
-  reduce _ xs = return xs
+  reduce xs _ = return xs
 
 runSelect :: FromBit a => Select a -> a
-runSelect (Selected x)   = x
-runSelect (Choice b x y) = choose (runSelect x) (runSelect y) (fromBit b)
+runSelect s = or (aux true s [])
+  where
+  aux sel (Selected x) z = (fromBit sel && x) : z
+  aux sel (Choice b x y) z =
+    aux (sel && not b) x (aux (sel && b) y z)
+
+-- This is cleaner but it seems to run more slowly, presumably
+-- due to doing more of the operations at 'a' instead of doing
+-- them on 'Bit'
+-- runSelect :: FromBit a => Select a -> a
+-- runSelect (Selected x)   = x
+-- runSelect (Choice b x y) = choose (runSelect x) (runSelect y) (fromBit b)
 
 instance Codec (Select a) where
 
