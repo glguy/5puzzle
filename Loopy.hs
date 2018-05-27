693951fa5dbd78f4c5925f9b7b89a7eb92a5f99a
Eric Mertens <emertens@gmail.com>
2018-05-26 19:49:11 -0700

    Loopy improvements

--- Loopy.hs
+++ Loopy.hs
@@ -1,41 +1,60 @@
 {-# Language RecordWildCards #-}
+{-|
+Module      : Main
+Description : Solver for the Loopy game
+Copyright   : (c) Eric Mertens, 2018
+License     : ISC
+Maintainer  : emertens@gmail.com
+
+Console-based solver for the Loopy game as defined at
+https://www.chiark.greenend.org.uk/~sgtatham/puzzles/js/loopy.html
+
+This solution enforces the constraint that a solution consists of
+active edges in the grid that form a single, continuous, non-intersecting
+loop by assigning each edge a direction and sequential ID. The final
+ID in the loop is permitted to connect back to the starting element.
+
+-}
 module Main where
 
-import Booleans
-import Control.Monad
-import Data.Char
-import Data.List (transpose)
-import Data.Map (Map)
-import Ersatz
-import Prelude hiding ((&&),(||),all, any, and, or, not)
-import SparseMap (SparseMap)
-import System.Environment
-import System.Exit
-import System.IO
+import           Booleans (MonadSAT, countBits)
+import           Control.Monad (replicateM)
+import           Data.Char (intToDigit, digitToInt, isDigit)
+import           Data.List (transpose)
+import           Data.Map (Map)
 import qualified Data.Map as Map
+import           Ersatz
+import           Prelude hiding ((&&),(||),all, any, and, or, not)
+import           SparseMap (SparseMap)
 import qualified SparseMap
+import           System.Environment (getArgs)
+import           System.Exit (exitFailure)
+import           System.IO (hPutStrLn, stderr)
 
 ------------------------------------------------------------------------
 -- Board edges
 ------------------------------------------------------------------------
 
+-- | Symbolic information about an edge on the board.
 data Edge = Edge
-  { edgeOn  :: Bit
-  , edgeFwd :: Bit
-  , edgeId  :: Bits
+  { edgeOn  :: Bit -- ^ true when this edge is active
+  , edgeFwd :: Bit -- ^ true when this edge points right or down
+  , edgeId  :: Bits -- ^ sequential ID number used to enforce a single loop
   }
 
+-- | Edge that is not active
 noEdge :: Edge
 noEdge = Edge false false 0
 
-newEdge :: MonadSAT s m => Int -> m Edge
+-- | Generate a new symbolic edge given a number of bits to use for the ID
+newEdge :: MonadSAT s m => Int {- ^ ID bits -} -> m Edge
 newEdge n =
   do edgeOn  <- exists
      edgeFwd <- exists
      edgeId  <- newBits n
      return Edge{..}
 
-newBits :: MonadSAT s m => Int -> m Bits
+newBits :: MonadSAT s m => Int {- ^ number of bits -} -> m Bits
 newBits n = Bits <$> replicateM n exists
 
 ------------------------------------------------------------------------
@@ -55,19 +74,25 @@ right (x,y) = (x+1,y)
 -- Board representation
 ------------------------------------------------------------------------
 
+-- | Flag for indicating a horizontal or vertical cell edge.
 data Orient = Horiz | Vert
   deriving (Eq, Ord, Read, Show)
 
+
 -- | Edges are labeled by association with the cell that the edge either
--- borders on top or left side.
+-- borders on top or left side. Edges point to the right and down.
 --
 -- @
--- +-
+-- +->
 -- |?
+-- v
 -- @
 type EdgeCoord = (Coord,Orient)
 
+
+-- | Symbolic representation of the edges on a board.
 type Board = SparseMap EdgeCoord Edge
+
 
 -- | Returns a list of edges that border the cell at the given coordinate.
 --
@@ -81,6 +106,7 @@ clueNeighborhood c =
   [            (c,Horiz),
     (c, Vert), {- c -} (right c,Vert),
                (down c,Horiz) ]
+
 
 -- | Returns a list of edges that touch the intersection in
 -- the top-left of a given cell coordinate. Each edge coordinate
@@ -100,6 +126,7 @@ edgeNeighborhood c =
   , ((left c, Horiz), false), {- + -} ((c, Horiz), true),
                           ((c, Vert), true) ]
 
+
 -- | Generate the list of locations of all edges on a board of the
 -- given width and height.
 locations ::
@@ -108,9 +135,10 @@ locations ::
   [EdgeCoord] {- ^ all edges on board -}
 locations w h = body ++ bottomEdge ++ rightEdge
   where
-    body       = [ ((x,y),o    ) | x <- [0..w-1], y <- [0..h-1], o <- [Horiz, Vert] ]
-    bottomEdge = [ ((x,h),Horiz) | x <- [0..w-1] ]
+    bottomEdge = [ ((x,h),Horiz) | x <- [0..w-1]                ]
     rightEdge  = [ ((w,y),Vert ) |                y <- [0..h-1] ]
+    body       = [ ((x,y),o    ) | x <- [0..w-1], y <- [0..h-1]
+                                 , o <- [Horiz, Vert]           ]
 
 ------------------------------------------------------------------------
 -- Puzzle representation
@@ -119,19 +147,20 @@ locations w h = body ++ bottomEdge ++ rightEdge
 -- | Puzzle parameters
 data Puzzle = Puzzle Int Int (Map Coord Int) -- ^ width height clues
 
+
 -- | Parse a puzzle from a text-file.
 parsePuzzle :: String -> Puzzle
-parsePuzzle str = Puzzle w (length ls) cells
-
+parsePuzzle str = Puzzle w h cells
   where
-    ls = lines str
-    w  = case ls of
-           x:_ -> length x
-           []  -> 0
-    cells = Map.fromList [ ((x,y),digitToInt n)
-                         | (y,l) <- zip [0..] ls
-                         , (x,n) <- zip [0..] l
-                         , isDigit n ]
+    ls    = lines str
+    h     = length ls
+    w     = maximum (0 : map length ls)
+    cells = Map.fromList
+              [ ((x,y),digitToInt n)
+                | (y,l) <- zip [0..] ls
+                , (x,n) <- zip [0..] l
+                , isDigit n ]
+
 
 -- | Render the solution to a puzzle as ASCII art.
 renderSolution ::
@@ -139,20 +168,42 @@ renderSolution ::
   SparseMap EdgeCoord Bool {- ^ solution          -} ->
   String                   {- ^ rendering         -}
 renderSolution (Puzzle w h clues) solution =
-  unlines $ concat
-  [ map concat $ transpose
-     [ [['+', if nh then '-' else ' ']
-      ,[if nv then '|' else ' ', maybe ' ' intToDigit (Map.lookup (x,y) clues)]]
+  rearrange
+  [ [ [[drawCorner eU eL eR eD, if eR then '─' else ' ']
+      ,[if eD then '│' else ' ',
+        drawClue x y (Map.lookup (x,y) clues)]]
      | x <- [0..w]
-     , let nh = SparseMap.index ((x,y),Horiz) solution
-     , let nv = SparseMap.index ((x,y),Vert ) solution
+     , let eR = SparseMap.index (     (x,y),Horiz) solution
+     , let eD = SparseMap.index (     (x,y),Vert ) solution
+     , let eL = SparseMap.index (left (x,y),Horiz) solution
+     , let eU = SparseMap.index (up   (x,y),Vert ) solution
         ]
      | y <- [0..h] ]
+  where
+    -- rows of cells of (lines in cell) to single string
+    rearrange :: [[[String]]] -> String
+    rearrange = unlines . concatMap (map concat . transpose)
+
+    drawClue _ _ (Just n) = intToDigit n
+    drawClue x y Nothing
+      | x < w && y < h = '·'
+      | otherwise      = ' ' -- right and bottom edge
+
+    drawCorner u l r d
+      | u, l = '┘'
+      | u, r = '└'
+      | d, l = '┐'
+      | d, r = '┌'
+      | u, d = '│'
+      | l, r = '─'
+      | True = ' '
 
 ------------------------------------------------------------------------
 -- Solver
 ------------------------------------------------------------------------
 
+-- | Given a puzzle determine an assignment of edges that satisfies
+-- the puzzle's clues unless one is impossible.
 solvePuzzleIO :: Puzzle -> IO (Maybe (SparseMap EdgeCoord Bool))
 solvePuzzleIO p =
   do result <- solveWith minisat (solvePuzzle p)
@@ -161,6 +212,9 @@ solvePuzzleIO p =
        (Unsatisfied, _)    -> return Nothing
        _                   -> fail "solvePuzzleIO: bad result"
 
+
+-- | Given a puzzle determine an assignment of edges that satisfies
+-- the puzzle's clues unless one is impossible.
 solvePuzzle :: MonadSAT s m => Puzzle -> m (SparseMap EdgeCoord Bit)
 solvePuzzle (Puzzle w h clues) =
   do (endId, cells) <- newCells w h
@@ -181,6 +235,7 @@ solvePuzzle (Puzzle w h clues) =
      -- returns map of active edges (forgetting edge IDs)
      return (fmap edgeOn cells)
 
+
 -- | Returns 'true' when at most one element in the given list-like structure
 -- is 'true'.
 atMostOne :: (Foldable t, Boolean a) => t a -> a
@@ -188,11 +243,14 @@ atMostOne = not . snd . foldl aux (false,false)
   where
     aux (one,two) x = (one || x, two || one && x)
 
+
 -- | Verify that the number in a particular cell matches the actual edges.
 validateClue :: Board -> Coord -> Int -> Bit
 validateClue board coord n = fromIntegral n === countBits localEdges
   where
-    localEdges = [ edgeOn (SparseMap.index c board) | c <- clueNeighborhood coord ]
+    localEdges = [ edgeOn (SparseMap.index c board)
+                   | c <- clueNeighborhood coord ]
+
 
 -- | Returns a list of the ways to partition a list of elements where
 -- the first list has the given length and the second list is the
@@ -204,23 +262,26 @@ partitions n (x:xs) =
   [ (x:l,r) | (l,r) <- partitions (n-1) xs ] ++
   [ (l,x:r) | (l,r) <- partitions n     xs ]
 
+
 -- | Check that any intersection is unused in the solution or that it
 -- is correct traversed by only two edges that are sequentially numbered.
 validateIntersection :: Bits -> Coord -> Board -> Bit
 validateIntersection endId coord board = emptyIntersection || activeIntersection
   where
-    noLine (e,_) = not (edgeOn e)
+    -- checks that none of the given edges are active
+    noEdges = all (not . edgeOn . fst)
 
-    values = map (\ (c,d) -> (SparseMap.index c board, d)) (edgeNeighborhood coord)
+    -- list of edges at this intersection and flag indicating outgoing edges
+    values = [ (SparseMap.index c board, d) | (c,d) <- edgeNeighborhood coord]
 
     -- check that none of the edges entering this intersection are active
-    emptyIntersection = all noLine values
+    emptyIntersection = noEdges values
 
     -- for any pair of edges that meet at this intersection check that
     -- the line enters along one edge and leaves along another such that
     -- the ID numbers of the edge are increasing.
     activeIntersection =
-      or [ all noLine others && edgeOn edge1 && edgeOn edge2 &&
+      or [ noEdges others && edgeOn edge1 && edgeOn edge2 &&
            (connected edge1 out1 edge2 out2 || connected edge2 out2 edge1 out1)
          | ([(edge1,out1),(edge2,out2)],others) <- partitions 2 values
          ]
@@ -229,9 +290,11 @@ validateIntersection endId coord board = emptyIntersection || activeIntersection
     -- of the edges if the smaller number is entering this intersection
     -- and the larger number is leaving it.
     connected edge1 out1 edge2 out2 =
-      edgeFwd edge1 /== out1 && edgeFwd edge2 === out2 &&
-      (edgeId edge1 + 1 === edgeId edge2 ||
-       edgeId edge1 === endId && edgeId edge2 === 0)
+      and [ edgeFwd edge1 /== out1
+          , edgeFwd edge2 === out2
+          , edgeId edge1 + 1 === edgeId edge2 ||         -- normal
+            edgeId edge1 === endId && edgeId edge2 === 0 -- wrap-around
+          ]
 
 -- | Generate a new board containing the edges available on a board
 -- with the given width and height. Also returns the number suitable
@@ -244,17 +307,20 @@ newCells ::
 newCells w h =
   do let keys       = locations w h
          bitsNeeded = ceiling (logBase 2 (fromIntegral (length keys)) :: Double)
-     cells <- sequence
-              (SparseMap.fromList (pure noEdge) [ (k,newEdge bitsNeeded) | k <- keys ])
      endId <- newBits bitsNeeded
+     cells <- sequence
+            $ SparseMap.fromList
+                (pure noEdge)
+                [ (k,newEdge bitsNeeded) | k <- keys ]
      return (endId, cells)
 
 main :: IO ()
 main =
   do puzzle <- parsePuzzle <$> getFile
-     res <- solvePuzzleIO puzzle
-     case res of
-       Just solution -> putStr (renderSolution puzzle solution)
+     result <- solvePuzzleIO puzzle
+     case result of
+       Just solution ->
+         do putStr (renderSolution puzzle solution)
        Nothing ->
          do putStrLn "No solution possible!"
             putStr (renderSolution puzzle (SparseMap.constant false))
@@ -265,7 +331,7 @@ getFile :: IO String
 getFile =
   do args <- getArgs
      case args of
-       ["-"] -> getContents
-       [fn] -> readFile fn
-       _    -> do hPutStrLn stderr "Usage: Loopy FILENAME"
-                  exitFailure
+       ["-"] -> do getContents
+       [fn ] -> do readFile fn
+       _     -> do hPutStrLn stderr "Usage: Loopy FILENAME"
+                   exitFailure
